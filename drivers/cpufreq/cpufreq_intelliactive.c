df5233e1355d8bd4d5e32d5d6a2b18e4602b1f18
evuz <jesus_jgb_45@hotmail.com>
2014-06-17 09:38:10 +0200

    Tunning Intelliactive

--- drivers/cpufreq/cpufreq_intelliactive.c
+++ drivers/cpufreq/cpufreq_intelliactive.c
@@ -65,7 +65,7 @@ static spinlock_t speedchange_cpumask_lock;
 static struct mutex gov_lock;
 
 /* Hi speed to bump to from lo speed when load burst (default max) */
-static unsigned int hispeed_freq = 1026000;
+static unsigned int hispeed_freq = 702000;
 
 /* Go to hi speed when CPU load at or above this value. */
 #define DEFAULT_GO_HISPEED_LOAD 99
@@ -130,7 +130,7 @@ static bool io_is_busy = 1;
  * sync_freq
  */
 static unsigned int up_threshold_any_cpu_load = 90;
-static unsigned int sync_freq = 1026000;
+static unsigned int sync_freq = 702000;
 static unsigned int up_threshold_any_cpu_freq = 1242000;
 
 static int two_phase_freq_array[NR_CPUS] = {[0 ... NR_CPUS-1] = 702000} ;
